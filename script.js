ad5712f8bcacb28b0ce6206c2fcc9f825127033b
Martim Costa <martimcosta95@gmail.com>
2024-01-30 16:19:54 +0000

    make the bar move

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ let barSize = 3;
 let isGameOver = false;
 let score = 0;
 
+// Draw the grid and the bar
 function draw() {
   // Reset the display when this function is called
   grid.innerHTML = '';
@@ -43,6 +44,42 @@ function draw() {
       grid.appendChild(cell);
     })
   })
+}
+
+function moveRight(currentRow) {
+  // Remove last element of the array
+  currentRow.pop();
+  // Insert new element at the beggining of the array
+  currentRow.unshift(0);
+}
+
+function moveLeft(currentRow) {
+  // Remove first element of the array
+  currentRow.shift();
+  // Insert new element at the end of the array
+  currentRow.push(0);
+}
+
+// Make the bar move
+function moveBar() {
+  const currentRow = gridMatrix[currentRowIndex];
+
+  if (barDirection === 'right') {
+    moveRight(currentRow);
+
+    const lastElement = currentRow[currentRow.length -1];
+    if (lastElement === 1) {
+      barDirection = 'left';
+    }
+
+  } else if (barDirection === 'left') {
+    moveLeft(currentRow);
+
+    const firstElement = currentRow[0];
+    if (firstElement === 1) {
+      barDirection = 'right';
+    }
+  }
 }
 
 draw();
